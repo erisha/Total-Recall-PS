f5776082f6fd5747ce5a472f26664dbe2c0db6d0
Erisha Roach <forever.erisha@gmail.com>
2023-11-21 09:47:19 -0500

    #1.A solved

---
+++ Section1/src/index.js
@@ -0,0 +1,13 @@
+////////////////////////////////
+// 1. Variable and Data Types
+////////////////////////////////
+
+// A. Variables
+
+const myName = 'Erisha';
+let favoriteLanguage = 'Python';
+let newFavoriteLanguage = 'Javascript';
+
+favoriteLanguage = newFavoriteLanguage; 
+
+console.log(`Hello my name is ${myName}. My favorite language is ${favoriteLanguage}.`);
